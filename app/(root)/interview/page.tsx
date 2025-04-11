153fe059a17f68637a131b16e5cbd53bea74c3ab
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-11 16:57:28 -0600

    feat: integrate user authentication in interview page component

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,16 @@
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) return <div>Loading...</div>;
+
   return (
     <>
-      <h3></h3>
-      <Agent userName="You" userId="user1" type="generate" />
+      <h3>Interview Generation</h3>
+      <Agent userName={user.name} userId={user.id} type="generate" />
     </>
   );
 };
