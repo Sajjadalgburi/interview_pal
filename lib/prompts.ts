3f2ad88c1a128e3bbd6f7ad93039299c17964037
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-11 15:29:48 -0600

    feat: add geminiPrompt function for generating interview questions

---
+++ lib/prompts.ts
@@ -0,0 +1,24 @@
+export const geminiPrompt = ({
+  role,
+  level,
+  techstack,
+  amount,
+  type,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}: any): string => {
+  const prompt = `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `;
+
+  return prompt;
+};
