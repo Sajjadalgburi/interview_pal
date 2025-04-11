bac97d1fa721a4d55774b7a3097805545becc31a
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-11 09:07:35 -0600

    feat: implement user registration in AuthForm with Firebase authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -35,13 +38,34 @@ const AuthForm = ({ type }: { type: FormType }) => {
       password: "",
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       // Means user is registering for the first time
       if (type === "sign-up") {
+        const { name, email, password } = values;
+
+        // first we would like to authenticate the user before creating a user
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password,
+        );
+
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
-        console.log("sign up", values);
       } else {
         // Means user is signing in /logging
         toast.success("Signed In successfully!");
