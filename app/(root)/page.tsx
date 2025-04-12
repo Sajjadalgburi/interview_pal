64ee53dfcd3ccef0b9fb19a599a91c89d53413b5
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-12 11:46:45 -0600

    feat: update interview page to fetch and display latest interviews for the user

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import {
   getCurrentUser,
-  getInterviewByUserId,
+  getInterviewsByUserId,
+  getLatestInterviews,
 } from "@/lib/actions/auth.action";
 import { redirect } from "next/navigation";
 
@@ -14,9 +14,13 @@ const Page = async () => {
   const user = await getCurrentUser();
   if (!user) redirect("/sign-in");
 
-  const userInterviews = await getInterviewByUserId(user.id);
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user.id),
+    await getLatestInterviews({ userId: user.id }),
+  ]);
 
   const hasPastInterviews: boolean = userInterviews?.length > 0;
+  const hasUpcomingInterviews: boolean = latestInterviews?.length > 0;
 
   return (
     <>
@@ -59,9 +63,15 @@ const Page = async () => {
       <section className="mt-8 flex flex-col gap-6">
         <h2>Take an interview</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard key={interview.id} {...interview} />
-          ))}{" "}
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard key={interview.id} {...interview} />
+            ))
+          ) : (
+            <p className="md:text-md text-sm">
+              You have not taken any interviews yet!
+            </p>
+          )}
         </div>
       </section>
     </>
