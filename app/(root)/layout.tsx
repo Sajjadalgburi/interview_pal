e0e1736550e8aae4d7a4c6672f35f984cea0368e
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-12 13:32:54 -0600

    feat: replace custom navigation with Navbar component in RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="root-layout">
-      <nav>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="logo" width={38} height={32} />
-          <h2 className="texct-primary-100">Interview Pal</h2>
-        </Link>
-      </nav>
-      {children}
-    </div>
+    <>
+      <Navbar />
+      <div className="root-layout">{children}</div>{" "}
+    </>
   );
 };
 
