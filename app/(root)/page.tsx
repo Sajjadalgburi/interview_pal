d4f6c87ebc1cb9a48fe19d8b6a7d96b567029ae2
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-04-12 14:14:10 -0600

    refactor: enhance Home component structure and improve interview display logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,81 +1,97 @@
-import InterviewCard from "@/components/InterviewCard";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import InterviewCard from "@/components/InterviewCard";
+
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import {
   getInterviewsByUserId,
   getLatestInterviews,
 } from "@/lib/actions/general.action";
 import { redirect } from "next/navigation";
-import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = async () => {
+async function Home() {
   const user = await getCurrentUser();
+
   if (!user) redirect("/sign-in");
 
-  const [userInterviews, latestInterviews] = await Promise.all([
-    await getInterviewsByUserId(user.id),
-    await getLatestInterviews({ userId: user.id }),
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id),
+    getLatestInterviews({ userId: user?.id }),
   ]);
 
-  const hasPastInterviews: boolean = userInterviews?.length > 0;
-  const hasUpcomingInterviews: boolean = latestInterviews?.length > 0;
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
 
   return (
     <>
-      <section className="card-cta mt-12">
+      <section className="card-cta">
         <div className="flex max-w-lg flex-col gap-6">
-          <h2>Be Interview-Ready with AI-Powered Mock Interviews</h2>
-          <p>
-            Practice interviewing with our AI-powered platform. Get real-time
-            feedback and improve your skills.
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
           </p>
+
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
+
         <Image
-          src={"/robot.png"}
-          alt="robot-image"
+          src="/robot.png"
+          alt="robo-dude"
           width={400}
           height={400}
-          className="object-cover max-md:hidden"
-        />{" "}
+          className="max-sm:hidden"
+        />
       </section>
 
       <section className="mt-8 flex flex-col gap-6">
         <h2>Your Interviews</h2>
+
         <div className="interviews-section">
-          {/* <p>You have not taken any interviews yet!</p> */}
           {hasPastInterviews ? (
             userInterviews?.map((interview) => (
-              <InterviewCard key={interview.id} {...interview} />
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                id={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
             ))
           ) : (
-            <p className="md:text-md text-sm">
-              You have not taken any interviews yet!
-            </p>
+            <p>You haven&apos;t taken any interviews yet</p>
           )}
         </div>
       </section>
 
       <section className="mt-8 flex flex-col gap-6">
-        <h2>Take an interview</h2>
+        <h2>Take Interviews</h2>
+
         <div className="interviews-section">
           {hasUpcomingInterviews ? (
-            latestInterviews?.map((interview) => (
-              <InterviewCard key={interview.id} {...interview} />
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                id={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
             ))
           ) : (
-            <p className="md:text-md text-sm">
-              You have not taken any interviews yet!
-            </p>
+            <p>There are no interviews available</p>
           )}
         </div>
       </section>
     </>
   );
-};
+}
 
-export default Page;
+export default Home;
